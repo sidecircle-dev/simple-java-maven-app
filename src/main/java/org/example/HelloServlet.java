a03f7a551f740bba7393229d2853ff0aba89839d
sidecircle-dev <73586140+sidecircle-dev@users.noreply.github.com>
2023-09-18 08:54:41 +0200

    Update HelloServlet.java

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -1,22 +1,20 @@
 // Reflecting the directory structure where the file lives
 package org.example;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
 public class HelloServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException
-    {
-        // Very simple - just return some plain text
-        PrintWriter writer = response.getWriter();
-        writer.print("Hello World");
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("<h1>Hello, World!</h1>");
     }
 }
-
