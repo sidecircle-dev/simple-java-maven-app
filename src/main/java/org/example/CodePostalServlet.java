ec539b9f3fe8fb181af8a91b67863815d62c9705
sidecircle-dev <73586140+sidecircle-dev@users.noreply.github.com>
2023-11-27 16:24:52 +0100

    Update CodePostalServlet.java

--- src/main/java/org/example/CodePostalServlet.java
+++ src/main/java/org/example/CodePostalServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "CodePostalServlet", urlPatterns = {"/ville"})
-public class HelloServlet extends HttpServlet {
+public class CodePostalServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
