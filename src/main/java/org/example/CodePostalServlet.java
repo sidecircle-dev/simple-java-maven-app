a7c9522155444586b55307334f6d1455a9778744
sidecircle-dev <73586140+sidecircle-dev@users.noreply.github.com>
2023-11-27 17:21:37 +0100

    Update CodePostalServlet.java using init and destroy to connect to pgresql

--- src/main/java/org/example/CodePostalServlet.java
+++ src/main/java/org/example/CodePostalServlet.java
@@ -1,17 +1,19 @@
 // Reflecting the directory structure where the file lives
 package org.example;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
+import java.io.IOException; 
+import java.io.PrintWriter; 
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import java.sql.ResultSet; 
+import java.sql.SQLException; 
+import java.sql.Statement; 
+  
+import javax.servlet.ServletException; 
+import javax.servlet.annotation.WebServlet; 
+import javax.servlet.http.HttpServlet; 
+import javax.servlet.http.HttpServletRequest; 
+import javax.servlet.http.HttpServletResponse; 
 
 @WebServlet(name = "CodePostalServlet", urlPatterns = {"/ville"})
 public class CodePostalServlet extends HttpServlet {
@@ -19,23 +21,37 @@ public class CodePostalServlet extends HttpServlet {
     private final String url = "jdbc:postgresql://localhost/codepostal";
     private final String user = "codepostal";
     private final String password = "codepostal";
+    private final String driver = "org.postgresql.Driver";
 
-    public Connection connect() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the PostgreSQL server successfully.");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
+    Connection conn = null; 
 
-        return conn;
-    }
-    
+    public void init() throws ServletException { 
+  
+        // Database connection through Driver Manager 
+        try { 
+            Class.forName(driver); 
+            conn = DriverManager.getConnection(url, user, password); 
+        } catch (ClassNotFoundException | SQLException e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+            
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        cnx = self.connect();
+                
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().println("<!DOCTYPE html> <html lang=\"en\"><head><title>title</title></head><body><h2>codepostal-ville!</h2></body></html>");
- }
+     }
+
+    public void destroy() { 
+  
+        // Close connection object. 
+        try { 
+            conn.close(); 
+        } catch (SQLException e) { 
+            e.printStackTrace(); 
+        } 
+    } 
 }
