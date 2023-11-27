0f7620e0be815e42084e0917558b94408ad4a7ae
sidecircle-dev <73586140+sidecircle-dev@users.noreply.github.com>
2023-11-27 17:09:44 +0100

    Update CodePostalServlet.java

--- src/main/java/org/example/CodePostalServlet.java
+++ src/main/java/org/example/CodePostalServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 @WebServlet(name = "CodePostalServlet", urlPatterns = {"/ville"})
 public class CodePostalServlet extends HttpServlet {
 
